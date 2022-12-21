539755da5b56c32e40dd852f6f37dbc9ac8738d8
seanevanlee <seanevanlee@gmail.com>
2022-12-21 12:54:39 -0800

    3 finches are up

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,14 +1,22 @@
 # from django.contrib import admin
 # # Add the include function to the import
 # from django.urls import path, include
-from django.contrib import admin
-from django.urls import path, include
+# from django.contrib import admin
+from django.urls import path
 
 from . import views
 
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', views.home, name='home'),
+#     path('about/', views.about, name='about'),
+#     # In this case '' represents the root route
+#     path('', include('main_app.urls')),
+# ]
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # In this case '' represents the root route
-    path('', include('main_app.urls')),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('finches/', views.finches_index, name='index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+
+
+class Finch:
+    def __init__(self, name, breed, sound, age):
+        self.name = name
+        self.breed = breed
+        self.sound = sound
+        self.age = age
+
+
+# This the array, we are injecting into the template
+finches = [
+    Finch('Geospiza magnirostris', 'brown', 'chirp chirp', 1),
+    Finch('Geospiza fortis', 'black', 'chirp chirp', 2),
+    Finch('Geospiza fuliginosa', 'red', 'chirp chirp', 3)
+]
+
+
+def home(request):
+    return HttpResponse('<h1>It is finching time!</h1>')
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {'finches': finches})
